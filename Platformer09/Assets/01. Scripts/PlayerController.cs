4bbab8de69046123b0a047e08272b03518b06ca0
SungJieundev <sungjieun.dev@gmail.com>
2022-09-15 14:28:59 +0900

    2022-09-15 PlayerController

--- Platformer09/Assets/01. Scripts/PlayerController.cs
+++ Platformer09/Assets/01. Scripts/PlayerController.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    
-    void Start()
-    {
-        
+    public float walkSpeed = 10f;
+    public float gravity = 20f;
+
+    private Vector2 _input;
+    private Vector2 _moveDir;
+    private CharacterController2D _characterController;
+
+    private void Awake() {
+        _characterController = GetComponent<CharacterController2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        _moveDir.x = _input.x * walkSpeed;
+        //_moveDir.y -= gravity * Time.deltaTime;
+        _characterController.Move(_moveDir * Time.deltaTime);
+    }
+
+    public void OnMovement(InputAction.CallbackContext context){
+        _input = context.ReadValue<Vector2>();        
     }
 }
