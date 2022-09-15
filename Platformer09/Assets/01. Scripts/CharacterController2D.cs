9c90c57bfe97929915aa606c301ef252b1bfea45
SungJieundev <sungjieun.dev@gmail.com>
2022-09-15 15:40:16 +0900

    2022-09-15 Jump

--- Platformer09/Assets/01. Scripts/CharacterController2D.cs
+++ Platformer09/Assets/01. Scripts/CharacterController2D.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController2D : MonoBehaviour
-{
+{   
+    public float raycastDistance = 0.2f;
+    public LayerMask layerMask;
+
+    // flags
+    public bool below;
+
     private Vector2 _moveAmount;
     private Vector2 _currentPosition;
     private Vector2 _lastPosition;
 
     private Rigidbody2D _rigidbody;
     private CapsuleCollider2D _capsuleCollider;
+
+    private Vector2[] _raycastPosition = new Vector2[3];
+    private RaycastHit2D[] _raycastHits = new RaycastHit2D[3];
 
     void Awake() {
         _rigidbody = GetComponent<Rigidbody2D>();
@@ -23,9 +32,44 @@ public class CharacterController2D : MonoBehaviour
         _rigidbody.MovePosition(_currentPosition); 
 
         _moveAmount = Vector2.zero;
+
+        CheckGrounded();
     }
 
     public void Move(Vector2 movement){
         _moveAmount += movement;
+    }
+
+    private void CheckGrounded(){
+        Vector2  raycastOrigin = _rigidbody.position - new Vector2(0, _capsuleCollider.size.y * 0.5f);
+        _raycastPosition[0] = raycastOrigin+(Vector2.left * _capsuleCollider.size.x * 0.25f + Vector2.up * 0.1f);
+        _raycastPosition[1] = raycastOrigin;
+        _raycastPosition[2] = raycastOrigin+(Vector2.right * _capsuleCollider.size.x * 0.25f + Vector2.up * 0.1f);
+
+        DrawDebugRays(Vector2.down, Color.green);
+
+        int numberOfGroundHits = 0;
+
+        for(int i = 0; i < _raycastPosition.Length; i++){
+            RaycastHit2D hit = Physics2D.Raycast(_raycastPosition[i], Vector2.down, raycastDistance, layerMask);
+
+            if(hit.collider){
+                _raycastHits[i] = hit;
+                numberOfGroundHits++;
+            }
+        }
+
+        if(numberOfGroundHits > 0){
+            below = true;
+        }
+        else{
+            below = false;
+        }
+    }
+
+    private void DrawDebugRays(Vector2 dir, Color color){
+        for(int i = 0; i < _raycastPosition.Length; i++){
+            Debug.DrawRay(_raycastPosition[i], dir * raycastDistance, color);
+        }
     }
 }
--- Platformer09/Assets/01. Scripts/PlayerController.cs
+++ Platformer09/Assets/01. Scripts/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 10f;
     public float gravity = 20f;
+    public float jumpSpeed = 15f;
+
+    // Player state
+    public bool isJumping;
+    private bool _startJump;
+    private bool _releaseJump;
 
     private Vector2 _input;
     private Vector2 _moveDir;
@@ -18,11 +24,38 @@ public class PlayerController : MonoBehaviour
 
     private void Update() {
         _moveDir.x = _input.x * walkSpeed;
-        //_moveDir.y -= gravity * Time.deltaTime;
+
+        if(_characterController.below){
+            if(_startJump){
+                _startJump = false;
+                _moveDir.y = jumpSpeed;
+                isJumping = true;
+            }
+        }
+        else{
+
+            if(_releaseJump){
+                _releaseJump = false;
+                
+                if(_moveDir.y > 0){
+                    _moveDir.y *= 0.5f;
+                }
+            }
+            _moveDir.y -= gravity * Time.deltaTime;
+        }
         _characterController.Move(_moveDir * Time.deltaTime);
     }
 
     public void OnMovement(InputAction.CallbackContext context){
         _input = context.ReadValue<Vector2>();        
+    }
+
+    public void OnJump(InputAction.CallbackContext context){
+        if(context.started){
+            _startJump = true;
+        }
+        else if(context.canceled){
+            _releaseJump = true;
+        }
     }
 }
